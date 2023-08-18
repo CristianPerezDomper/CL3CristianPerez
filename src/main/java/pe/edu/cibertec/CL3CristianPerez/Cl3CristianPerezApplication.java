a78d5bb030d61206ed7ae2387d53885bd4eed5c6
CristianPerezDomper <domperz@hotmail.com>
2023-08-17 21:55:28 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/CL3CristianPerez/Cl3CristianPerezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL3CristianPerez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl3CristianPerezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl3CristianPerezApplication.class, args);
+	}
+
+}
